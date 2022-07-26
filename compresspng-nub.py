430284d4b30f4fdd0d7776cc52f8aa8f90269eee
strongpapazola <strongpapazola@gmail.com>
2022-07-27 00:21:45 +0700

    'ong

--- compresspng-nub.py
+++ compresspng-nub.py
@@ -1,5 +1,24 @@
 from PIL import Image
-img_path = "asdasdasd.png"
-img = Image.open(img_path)
-img = img.convert("P", palette=Image.ADAPTIVE, colors=256)
-img.save("asdasdasd2.png", optimize=True)
+from subprocess import Popen, PIPE
+import time
+import sys
+
+def shell(cmd):
+ return Popen(cmd, shell=True, stdout=PIPE).stdout.read().decode('utf-8')
+
+a = shell("find * -type f | grep png").splitlines()
+lena = len(a)
+for i in range(0, lena):
+# sys.stdout.write(str(lena))
+# sys.stdout.flush()
+ print(str(lena)+" : "+str(a[i]), end='\r')
+ try:
+  im = Image.open(str(a[i]))
+  im = im.convert("P", palette=Image.ADAPTIVE, colors=256)
+  im.save(str(a[i]), optimize=True)
+#   im.save(str(a[i]), format="JPEG", quality=70)
+ except:
+  pass
+ lena = lena - 1
+
+# print(i)
